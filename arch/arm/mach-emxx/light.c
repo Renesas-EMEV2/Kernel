a0e75f1dd1b600a2b60bf109f6a07d883a33268f
Fabio Fumi <ffumi68@gmail.com>
2012-12-28 16:32:14 +0100

    Backlight PWM support

--- arch/arm/mach-emxx/light.c
+++ arch/arm/mach-emxx/light.c
@@ -27,22 +27,37 @@
 #include <linux/leds.h>
 
 #include <linux/gpio.h>
+#include <mach/pwm.h>
+#include "pwm.h"
 
 #define DEFAULT_BACKLIGHT_BRIGHTNESS 255
-#define HW_MAX_BRIGHTNESS 95
-#define HW_MIN_BRIGHTNESS 16
+#define HW_MAX_BRIGHTNESS 255
+#define HW_MIN_BRIGHTNESS 0
+
+struct emxx_pwm_cmpcnt_t pwm_cmpcnt;
 
 /*******************************/
 /* LCD BackLight */
 static void
 emxx_brightness_set(struct led_classdev *led_cdev, enum led_brightness value)
 {
+#if 0
 	int hw_val = (value * HW_MAX_BRIGHTNESS) / 255;
 
 	if (hw_val < HW_MIN_BRIGHTNESS)
 		hw_val = HW_MIN_BRIGHTNESS;
 	if (hw_val > HW_MAX_BRIGHTNESS)
 		hw_val = HW_MAX_BRIGHTNESS;
+#endif
+
+	pwm_cmpcnt.trail_edge = (1995*(HW_MAX_BRIGHTNESS-value+HW_MIN_BRIGHTNESS))/HW_MAX_BRIGHTNESS;
+	if(pwm_cmpcnt.trail_edge < pwm_cmpcnt.lead_edge)
+		pwm_cmpcnt.trail_edge = pwm_cmpcnt.lead_edge;
+	//pwm_cmpcnt.trail_edge = pwm_cmpcnt.total_cycle - pwm_cmpcnt.lead_edge;
+	pwm_cmpcnt.loop_count = 0;
+	emxx_pwm_stop(0);
+	emxx_pwm_set_compare_counter(0, &pwm_cmpcnt);
+	emxx_pwm_start(0);
 }
 
 static struct led_classdev emxx_backlight_led = {
@@ -81,6 +96,25 @@ static struct platform_device emxx_leds = {
 /*******************************/
 static int emxx_light_probe(struct platform_device *pdev)
 {
+	struct emxx_pwm_ch_config_t ch_config;
+	pwm_cmpcnt.cb_info = NULL;
+	emxx_pwm_get_channel_config(0, &ch_config);
+	emxx_pwm_get_compare_counter(0, &pwm_cmpcnt);
+
+	ch_config.mode = 0;
+	ch_config.use_cmp = PWM_CMP_EN0_BIT;
+	emxx_pwm_set_channel_config(0, &ch_config);
+
+	pwm_cmpcnt.cmp = EMXX_PWM_COMPARE0;
+	pwm_cmpcnt.delay = 0;
+	pwm_cmpcnt.lead_edge = 5;
+	pwm_cmpcnt.trail_edge = 1200;
+	pwm_cmpcnt.total_cycle = 2000;		//about 14.2KHz
+	pwm_cmpcnt.loop_count = 0;
+	pwm_cmpcnt.cb_info = NULL;
+	emxx_pwm_set_compare_counter(0, &pwm_cmpcnt);
+
+	emxx_pwm_start(0);
 	/* Init LCD Backlight */
 	led_classdev_register(&pdev->dev, &emxx_backlight_led);
 
